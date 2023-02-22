9201036bca238914ba04dcadb12780bef841fc4a
Clemens Stift <clemens.stift@bitpanda.com>
2023-02-22 10:03:23 +0100

    Consume prices from Kafka with Coroutine.

---
+++ trading/src/main/kotlin/io/squer/ports/incoming/PriceListener.kt
@@ -0,0 +1,40 @@
+package io.squer.ports.incoming
+
+import io.micronaut.configuration.kafka.annotation.KafkaListener
+import io.micronaut.configuration.kafka.annotation.Topic
+import io.squer.ports.outgoing.prices.AssetPriceEntity
+import io.squer.ports.outgoing.prices.AssetPriceRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.GlobalScope.coroutineContext
+import kotlinx.coroutines.launch
+import reactor.core.publisher.Mono
+import java.math.BigDecimal
+import java.time.LocalDateTime
+import java.util.UUID
+
+@KafkaListener
+class PriceListener(
+    private val assetPriceRepository: AssetPriceRepository
+) {
+    @Topic(SQUER_TRADING_PRICES)
+    fun receivePrice(price: Price): Mono<Unit> {
+        return Mono.fromCallable {
+            CoroutineScope(coroutineContext).launch {
+                assetPriceRepository.save(
+                    AssetPriceEntity(
+                        id = UUID.randomUUID(),
+                        price = price.price,
+                        assetId = price.assetId,
+                        createdAt = LocalDateTime.now()
+                    )
+                )
+            }
+        }
+    }
+
+    companion object {
+        const val SQUER_TRADING_PRICES = "squer-trading-prices"
+    }
+}
+
+data class Price(val assetId: UUID, val price: BigDecimal, val createdAt: LocalDateTime)
